f41578e62b9d5a523723e9d41d7ebaf6e97c7a16
omerwyo <omerbaggia123@gmail.com>
2021-10-06 16:10:32 +0800

    removed DB lines

--- LocusAPI/__init__.py
+++ LocusAPI/__init__.py
@@ -1,15 +1,15 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 
 import LocusAPI.routes.admin
 import LocusAPI.routes.tag
 
 app = Flask(__name__)
 
-app.config['DATABASE_URL'] = 'sqlite:///users.sqlite3'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
+# app.config['DATABASE_URL'] = 'sqlite:///users.sqlite3'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#
+# db = SQLAlchemy(app)
 
 
 
