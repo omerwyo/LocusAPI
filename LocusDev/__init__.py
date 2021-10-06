128d09a60a1abade0d02aa6748cdc863cfa2614a
omerwyo <omerbaggia123@gmail.com>
2021-10-06 17:40:35 +0800

    change imports

--- LocusDev/__init__.py
+++ LocusDev/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 # from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+
 import LocusDev.routes.admin
 import LocusDev.routes.tag
 
-app = Flask(__name__)
 
 # app.config['DATABASE_URL'] = 'sqlite:///users.sqlite3'
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
