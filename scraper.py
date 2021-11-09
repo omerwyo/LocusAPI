6641a0a270b2bd4272dfa3dbb020269afb832d1b
omerwyo <omerbaggia123@gmail.com>
2021-11-09 17:34:34 +0800

    change time

--- scraper.py
+++ scraper.py
@@ -20,7 +20,7 @@ import time
 scheduler = APScheduler()
 
 # @scheduler.task("interval", id="wrapper", hours=4, misfire_grace_time=900)
-@scheduler.task("cron", id="wrapper", hour='9', minute='35')
+@scheduler.task("cron", id="wrapper", hour='9', minute='37')
 def wrapperTask():
     parseMOHFeed()
     time.sleep(5)
