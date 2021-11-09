45223187b2a8f591d18a953281d34b2f94c2eb13
omerwyo <omerbaggia123@gmail.com>
2021-11-09 22:37:49 +0800

    Typo

--- App.py
+++ App.py
@@ -29,7 +29,7 @@ def create_app(test_config=None):
 
 app = create_app()
 
-@scheduler.task("cron", id="wrapper", hour='14', minute='39')
+@scheduler.task("cron", id="wrapper", hour='14', minute='41')
 def wrapperTask():
     parseMOHFeed()
     time.sleep(5)
