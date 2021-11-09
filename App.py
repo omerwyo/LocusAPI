45e08b2cfd07cb084c08efc52ddd140db943899d
omerwyo <omerbaggia123@gmail.com>
2021-11-09 23:15:46 +0800

    admin shift

--- App.py
+++ App.py
@@ -2,7 +2,7 @@ import logging
 from flask import Flask, redirect, abort, request, jsonify
 from flask_apscheduler import APScheduler
 import time
-# from models import db
+from models import db
 from sqlalchemy import desc
 from models import Article
 from scraper import parseMOHFeed, gov_sg_api_scrape, checkTags
@@ -15,7 +15,7 @@ def create_app(test_config=None):
     app = Flask(__name__)
     setup_db(app)
     """ uncomment at the first time running the app """
-    db_drop_and_create_all()
+    # db_drop_and_create_all()
 
     # @app.errorhandler(500)
     # def server_error(error):
@@ -45,13 +45,65 @@ def dailyUpdates():
     logging.info("data sent for evaluation {}".format(data))
     # result = parseMOHFeed()
     print("CHECK FOR PROBLEM")
-    # entities = Article.query.order_by(desc(Article.datePublished)).all()
-    entities = Article.query.all()
+    entities = Article.query.order_by(desc(Article.datePublished)).all()
+    # entities = Article.query.all()
     print("ENTITIES")
     logging.info('RETURNING DB STUFF WORKS')
     # logging.info("My result :{}".format(result))
     response = jsonify(json_list=[e.serialize for e in entities])
     return response
+
+
+# add some sort of authorisation, where the API key is needed for administrators to post up info
+@app.route('/admin', methods=['POST'])
+def adminNew():
+    if not request.args.get('API_KEY') or request.args.get('API_KEY') != os.environ.get('LOCUS_API_KEY'): return jsonify({'message':'Please use a valid API Key!'}), 401
+    data = request.get_json()
+    newArticle = Article(articleId= data['articleLink'],
+                         title=data['title'],
+                         bodyText=data['bodyText'],
+                         datePublished=datetime.datetime.now(),
+                         description="")
+    if data['articleLink']: newArticle.articleId = data['articleLink']
+    if data['articleDescription']: newArticle.description = data['articleDescription']
+
+    db.session.add(newArticle)
+    db.session.commit()
+
+    logging.info("Data sent for evaluation {}".format(data))
+    result = ""
+    logging.info("My result :{}".format(result))
+    return json.dumps(result)
+
+@app.route('/admin/update', methods=['PUT'])
+def adminPut():
+    if not request.args.get('API_KEY') or request.args.get('API_KEY') != os.environ.get('LOCUS_API_KEY'): return jsonify({'message': 'Please use a valid API Key!'}), 401
+    if not request.args.get('articleId'): return jsonify({'message': 'Please specify an articleId to update!'}), 400
+
+    article = Article.query.filter_by(articleId=request.args.get('articleId').strip()).first()
+    if not article: return jsonify({'message': 'No article found with specified ID.'}), 400
+
+    data = request.get_json()
+    article.title = data['title']
+    article.bodyText = data['bodyText']
+    article.articleId = data['articleLink']
+    # article.datePublished = data['articleLink']
+    # article.description = data['description']
+
+    db.session.commit()
+    return jsonify({'message': 'Article updated successfully.'}), 200
+
+@app.route('/admin/delete', methods=['DELETE'])
+def adminDelete():
+    if not request.args.get('API_KEY') or request.args.get('API_KEY') != os.environ.get('LOCUS_API_KEY'): return jsonify({'message': 'Please use a valid API Key!'}), 401
+    if not request.args.get('articleId'): return jsonify({'message': 'Please specify an articleId to delete!'}), 400
+
+    article = Article.query.filter_by(articleId=request.args.get('articleId').strip()).first()
+    db.session.delete(article)
+    db.session.commit()
+
+    return jsonify({'message': 'Article deleted successfully.'}), 200
+
 
 @app.route('/', methods=['GET'])
 def default_route():
